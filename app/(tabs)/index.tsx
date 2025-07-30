818b30f89c642651093b267f37b047da088a2915
Iliarocks <parun.ilia@gmail.com>
2025-07-30 13:01:47 -0700

    Derp

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,13 @@
 import { Text, View, StyleSheet } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
-    <View
-      style={styles.container}
-    >
-      <Text style={styles.text}>Home screen</Text>
+    <View style={styles.container}>
+	<Text style={styles.text}>Home screen</Text>
+	<Link href="/about" style={styles.button}>
+	   Go to About screen
+	</Link>
     </View>
   );
 }
@@ -18,6 +20,11 @@ const styles = StyleSheet.create({
 	justifyContent: "center",
     },
     text: {
+	color: '#ffffff',
+    },
+    button: {
+	fontSize: 20,
+	textDecorationLine: 'underline',
 	color: '#ffffff',
     },
 });
---
+++ constants/Colors.ts
@@ -0,0 +1,7 @@
+export default {
+    background: "#0E0E0E",
+    primary: "#161616",
+    secondary: "#101010",
+    text-primary: "#CECECE",
+    text-secondary: "#5A5A5A",
+}

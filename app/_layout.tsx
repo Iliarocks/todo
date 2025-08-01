1a54f191da9ec22b973c5308b3cb31d63b5a9b05
Iliarocks <parun.ilia@gmail.com>
2025-07-31 17:55:09 -0700

    Added persistent add button

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,19 @@
 import { Stack } from "expo-router";
+import { View, SafeAreaView } from "react-native";
+import Button from "@/components/Button";
+import "../assets/global.css";
 
 export default function RootLayout() {
   return (
-    <Stack>
-	<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <SafeAreaView className="flex-1 bg-background">
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      ></Stack>
+      <View className="bottom-safe right-0 absolute px-xl py-3xs">
+        <Button type="icon" label="add" />
+      </View>
+    </SafeAreaView>
   );
 }

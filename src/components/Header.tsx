53b7349706c19c01d9b03a622dae05f6d3a952b0
Iliarocks <parun.ilia@gmail.com>
2025-08-08 16:06:19 -0700

    centered content and made children optional

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,11 @@
-import { View } from "react-native";
-import CustomText from "./CustomText";
+import Text from "@/components/Text";
 import { ReactNode } from "react";
+import { View } from "react-native";
 
-export default function Header({ children }: { children: ReactNode }) {
+export default function Header({ children }: { children?: ReactNode }) {
   return (
-    <View className="py-lg">
-      <CustomText color="secondary">{children}</CustomText>
+    <View className="justify-center py-lg">
+      <Text style="primary">{children}</Text>
     </View>
   );
 }

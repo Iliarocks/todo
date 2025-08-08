64151be76e6e3efeeba1386348ce056f595f1072
Iliarocks <parun.ilia@gmail.com>
2025-08-08 09:05:00 -0700

    Rewrote component to no children form

--- components/CheckBox.tsx
+++ components/CheckBox.tsx
@@ -24,7 +24,7 @@ export default function CheckBox({ onCheck }: { onCheck: () => void }) {
         style={{
           backgroundColor: checkBoxInterpolation,
         }}
-      ></Animated.View>
+      />
     </Pressable>
   );
 }

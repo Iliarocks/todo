5dba24b2cb48a7fe8cdc482b646ceeff403cf123
iliarocks <parun.ilia@gmail.com>
2025-08-14 15:15:13 -0700

    require -> import

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,11 @@ import { FONT } from "./src/constants/font";
 import { FONT_SIZE } from "./src/constants/font-size";
 import { LINE_HEIGHT } from "./src/constants/line-height";
 import { SPACING } from "./src/constants/spacing";
+import presets from "nativewind/preset";
 
 module.exports = {
   content: ["./src/app/**/*.{ts,tsx}", "./src/components/**/*.{ts,tsx}"],
-  presets: [require("nativewind/preset")],
+  presets,
   theme: {
     extend: {
       colors: COLOR,
